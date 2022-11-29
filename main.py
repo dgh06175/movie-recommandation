67cc26afc1bc8878fa89eb5509fac21c7a643bb1
dgh06175 <dgh06175@gmail.com>
2022-11-29 22:43:26 +0900

    refactor: main.py 추가 주석 작성

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from model import calculate as ca
 
 # 전체 프로그램을 실행하는 메인 함수
 def run():
-    Output.print_guide()
-    Output.print_famous_movies()
-    user_movie_numbers = Input.input_movies()
-    genre_weight = Input.input_genre_importance()
-    result = ca.calculate_correlation(user_movie_numbers, genre_weight)
-    Output.printResult(result)
+    Output.print_guide() # 초기 화면 출력
+    Output.print_famous_movies() # 유명 영화 10개 출력
+    user_movie_numbers = Input.input_movies() # 사용자에게 유명 영화중에 몇개 입력받음
+    genre_weight = Input.input_genre_importance() # 사용자에게 장르 유사성을 추천 알고리즘에 적용할것인지 입력받음
+    result = ca.calculate_correlation(user_movie_numbers, genre_weight) # 상관관계 계산한 결과값 계산하여 상위 5개 result 변수에 저장
+    Output.printResult(result) # 결과 출력
 
 run()
