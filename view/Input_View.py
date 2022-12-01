f54bfab683a071201c29f5406babecc269ae5734
dgh06175 <dgh06175@gmail.com>
2022-12-01 20:45:59 +0900

    docs: 주석 달음

--- view/Input_View.py
+++ view/Input_View.py
@@ -1,29 +1,22 @@
-# Input_View : 사용자에게 어떤 입력을 받을때 사용하는 함수들을 모아둔 파일
+from . import View_constant as STRING
 
-# 상수 저장한 변수들
-MOVIE_INPUT_GUIDE = "\n입력 : "
-GENRE_INPUT_GUIDE = "\n비슷한 장르로 추천할까요? (Y/N) : "
-YES_CHAR = 'Y'
-NO_CHAR = 'N'
-EMPTY = ''
-GENRE_WEIGHT = {
-	"HEAVY" : 0.3,
-	"NORMAL" : 0.15,
-	"LIGHT" : 0.1
-} # 장르 중요성 가산 점수들
+'''
+Input_View : 사용자에게 어떤 입력을 받을때 사용하는 함수들을 모아둔 파일
 
+모두 직접 작성했다.
+'''
 
 # 사용자에게 유명 영화 목록중에 마음에 드는 영화들의 번호를 입력받고 그 값을 반환하는 함수
 def input_movies():
-	user_movie_numbers = list(map(int, input(MOVIE_INPUT_GUIDE).split()))
+	user_movie_numbers = list(map(int, input(STRING.MOVIE_INPUT_GUIDE).split()))
 	return user_movie_numbers
 
 # 사용자에게 장르의 유사도를 중요하게 생각하는지 아닌지 입력받고 그 값을 반환하는 함수
 def input_genre_importance():
-    genre_importance = input(GENRE_INPUT_GUIDE)
-    if genre_importance == YES_CHAR:
-        return GENRE_WEIGHT["HEAVY"]
-    if genre_importance == NO_CHAR:
-        return GENRE_WEIGHT["LIGHT"]
+    genre_importance = input(STRING.GENRE_INPUT_GUIDE)
+    if genre_importance == STRING.YES_CHAR:
+        return STRING.GENRE_WEIGHT["HEAVY"]
+    if genre_importance == STRING.NO_CHAR:
+        return STRING.GENRE_WEIGHT["LIGHT"]
     else:
-        return GENRE_WEIGHT["NORMAL"]
+        return STRING.GENRE_WEIGHT["NORMAL"]
--- view/Output_View.py
+++ view/Output_View.py
@@ -1,54 +1,37 @@
-# Output_View : 화면에 어떤 것들을 출력하는 함수들을 모아둔 파일
+from . import View_constant as STRING
 
-# 상수 저장한 변수들
-FAMOUS_MOVIE_NAMES = [
-    "Fight Club",
-    "Iron Man",
-    "The Dark Knight",
-    "Forrest Gump",
-    "The Matrix",
-    "Pirates of the Caribbean: The Curse of the Black Pearl",
-    "Star Wars",
-    "Twilight",
-    "Spider-Man 3",
-    "Titanic"
-]
-DIVIDE_LINE = '#' * 101
-MOVIE_NUM_INPUT_GUIDE = "#  다음 영화들중에 재미있게 봤거나, 볼 생각이 있는 영화들의 번호를 공백으로 구분하여 입력해주세요.  #"
-LOADING_TEXT = '\n계산 중입니다...'
-RESULT_GUIDE_TEXT = "\n추천 하는 5개의 영화 목록 입니다."
-TITLE_STR = 'title'
-SCORE_STR = 'score'
-GENRES_STR = 'genres'
-OVERVIEW_STR = 'overview'
+'''
+Output_View : 화면에 어떤 것들을 출력하는 함수들을 모아둔 파일
 
+모두 직접 작성했다.
+'''
 
 # 프로그램 실행하자마자 처음 가이드 출력
 def print_guide():
-    print(DIVIDE_LINE)
-    print(MOVIE_NUM_INPUT_GUIDE)
-    print(DIVIDE_LINE + '\n')
+    print(STRING.DIVIDE_LINE)
+    print(STRING.MOVIE_NUM_INPUT_GUIDE)
+    print(STRING.DIVIDE_LINE + '\n')
 
 # 유명한 영화 목록 출력
 def print_famous_movies():
-	for index in range(len(FAMOUS_MOVIE_NAMES)):
-		print(f"{index + 1}. {FAMOUS_MOVIE_NAMES[index]}")
+	for index in range(len(STRING.FAMOUS_MOVIE_NAMES)):
+		print(f"{index + 1}. {STRING.FAMOUS_MOVIE_NAMES[index]}")
 
 # 계산할때 로딩문구 출력
 def printLoading():
-    print(LOADING_TEXT)
+    print(STRING.LOADING_TEXT)
 
 # 결과값을 인자로 받고 최종 결과창 출력
 def printResult(result):
-    print(RESULT_GUIDE_TEXT)
+    print(STRING.RESULT_GUIDE_TEXT)
     print("\n   {:<38} {:<7} {:<18} {:<60}".format('제목', '연관성', '장르', '줄거리'))
     for i in range(len(result)):
-        title = result[i][TITLE_STR][:38]
-        correlation = "{:.2f}".format(result[i][SCORE_STR])
-        genre = result[i][GENRES_STR][0]
-        if len(result[i][GENRES_STR])>=2:
-            genre += f", {result[i][GENRES_STR][1]}"
+        title = result[i][STRING.TITLE][:38]
+        correlation = "{:.2f}".format(result[i][STRING.SCORE])
+        genre = result[i][STRING.GENRES_STR][0]
+        if len(result[i][STRING.GENRES_STR])>=2:
+            genre += f", {result[i][STRING.GENRES_STR][1]}"
         genre = genre[:20]
-        overview = result[i][OVERVIEW_STR][:60]
+        overview = result[i][STRING.OVERVIEW_STR][:60]
         print("{:<1}. {:<40} {:<10} {:<20} {:<60}..".format(i + 1, title, correlation, genre, overview))
     print()
