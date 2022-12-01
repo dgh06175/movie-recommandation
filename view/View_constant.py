8e3ad8c095a0821c6ab06732cdfb458a9b85d3b7
dgh06175 <dgh06175@gmail.com>
2022-12-01 20:46:39 +0900

    refactor: view 파일들의 상수들을 따로 파일로 정리함

---
+++ view/View_constant.py
@@ -0,0 +1,38 @@
+# Input_View 파일에서 사용할 상수 저장한 변수들
+MOVIE_INPUT_GUIDE = "\n입력 : "
+GENRE_INPUT_GUIDE = "\n비슷한 장르로 추천할까요? (Y/N) : "
+YES_CHAR = 'Y'
+NO_CHAR = 'N'
+EMPTY = ''
+GENRE_WEIGHT = {
+	"HEAVY" : 0.3,
+	"NORMAL" : 0.15,
+	"LIGHT" : 0.1
+} # 장르 중요성 가산 점수들
+
+# Output_View 파일에서 사용할 상수 저장한 변수들
+FAMOUS_MOVIE_NAMES = [
+    "Fight Club",
+    "Iron Man",
+    "The Dark Knight",
+    "Forrest Gump",
+    "The Matrix",
+    "Pirates of the Caribbean: The Curse of the Black Pearl",
+    "Star Wars",
+    "Twilight",
+    "Spider-Man 3",
+    "Titanic"
+]
+DIVIDE_LINE = '#' * 101
+MOVIE_NUM_INPUT_GUIDE = "#  다음 영화들중에 재미있게 봤거나, 볼 생각이 있는 영화들의 번호를 공백으로 구분하여 입력해주세요.  #"
+LOADING_TEXT = '\n계산 중입니다...'
+RESULT_GUIDE_TEXT = "\n추천 하는 5개의 영화 목록 입니다."
+GENRES_STR = 'genres'
+OVERVIEW_STR = 'overview'
+
+# main.py에서 사용할 상수 저장한 변수들
+TITLE = 'title'
+SCORE = 'score'
+USERID = 'userId'
+ORIGINAL_TITLE = 'original_title'
+RATING = 'rating'
