10c64ed8d31d618c41f4bf156cec8b43f9ed5095
dgh06175 <dgh06175@gmail.com>
2022-12-01 20:44:39 +0900

    refactor: model 파일들의 상수들을 파일로 따로 정리함

---
+++ model/Model_constant.py
@@ -0,0 +1,38 @@
+# Correlation.py에서 사용할 상수 저장한 변수들
+MOVIE_DATASET_LOCATION = 'the-movies-dataset/movies_metadata.csv'
+RATING_DATASET_LOCATION = 'the-movies-dataset/ratings_small.csv'
+NAME_STR = 'name'
+TITLE_STR = 'title'
+GENRES_STR = 'genres'
+OVERVIEW_STR = 'overview'
+ID_STR = 'id'
+USER_ID_STR = 'userId'
+MOVIE_ID_STR = 'movieId'
+ORIGINAL_TITLE_STR = 'original_title'
+ORIGINAL_LANGUAGE_STR = 'original_language'
+RATING_STR = 'rating'
+SCORE_STR = 'score'
+POLULARITY_STR = 'popularity'
+COERCE_STR = 'coerce'
+QUOTE_STR = '\''
+DOUBLE_QUOTE_STR = '"'
+VOTE_AVERAGE_STR = 'vote_average'
+
+# Calculate.py에서 사용할 상수 저장한 변수들
+TITLE = 'title'
+USERID = 'userId'
+ORIGINAL_TITLE = 'original_title'
+RATING = 'rating'
+SCORE = 'score'
+FAMOUS_MOVIE_NAMES = [
+    "Fight Club",
+    "Iron Man",
+    "The Dark Knight",
+    "Forrest Gump",
+    "The Matrix",
+    "Pirates of the Caribbean: The Curse of the Black Pearl",
+    "Star Wars",
+    "Twilight",
+    "Spider-Man 3",
+    "Titanic"
+]
